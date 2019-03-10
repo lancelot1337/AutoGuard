8217cd03af7927590ef3a160ecb7d9bc9f50d101
nipunjain099 <nipunjain099@gmail.com>
2019-03-10 09:50:55 +0530

    Add app

---
+++ App/autoGuard/app/src/main/java/com/example/android/autoguard/perapp.java
@@ -0,0 +1,13 @@
+package com.example.android.autoguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class perapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perapp);
+    }
+}
